5926deb79761f696061d3dc7c2b53865b22c3174
Belal Khaleel <119034702+BelalKhaleel@users.noreply.github.com>
2026-04-24 13:02:45 +0300

    Split webpack.config.js into webpack.common.js, webpack.dev.js, and webpack.prod.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,21 +1,23 @@
 import path from "node:path";
+ import { fileURLToPath } from "node:url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default {
-  mode: "development",
-  entry: "./src/index.js",
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
+  entry: {
+    app: "./src/index.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
+      title: "Production",
       template: "./src/template.html",
     }),
   ],
   output: {
-    filename: "main.js",
-    path: path.resolve(import.meta.dirname, "dist"),
+    filename: "main.bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   module: {
